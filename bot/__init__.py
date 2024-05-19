c0df3590b794b5b9ba9db1c2e10bf7e6c849bf14
yoourlikeee <yoourlikeee@gmail.com>
2024-05-19 11:28:28 +0530

    __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "24123408"))
+    API_HASH = os.environ.get("API_HASH", "31b8525e9486aed13fc6d8cf9473cd4b")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6040448297:AAEQ_lG3UbUmjD0ftk50rA0SVrH26-szAm0")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "RadheRaniMusic_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
